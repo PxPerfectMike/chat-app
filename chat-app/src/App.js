144a75dde789ce2e00d5e752547b450708ce7e7c
Michael Baker <pxperfectmike@gmail.com>
2022-04-20 15:51:28 -0700

    connect react to firebase backend

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { getAnalytics } from "firebase/analytics";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA8rjHsVGGywTLajNICgSuy2pd_rklU6s4",
+  authDomain: "chat-app-px.firebaseapp.com",
+  projectId: "chat-app-px",
+  storageBucket: "chat-app-px.appspot.com",
+  messagingSenderId: "131974542331",
+  appId: "1:131974542331:web:ead90e3f44c47b413c115e",
+  measurementId: "G-2N8DCH8NFS"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
