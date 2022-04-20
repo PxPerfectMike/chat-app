f854ddafd1ae26eed5e6fdaca66ec88d77fc632c
Michael Baker <pxperfectmike@gmail.com>
2022-04-20 16:10:48 -0700

    add signin/signout flow

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -23,13 +23,41 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
 
       </header>
+      <section >
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
     </div>
   );
 }
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {}
 
 export default App;
